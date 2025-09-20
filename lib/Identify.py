48eac0a8a8f02af83a1a869b9f6083d3d21ec305
SpicyCuban <jessedenadel0@gmail.com>
2025-09-20 22:08:05 +0200

    Update Identify.py

--- lib/Identify.py
+++ lib/Identify.py
@@ -14,6 +14,7 @@ import socket
 from datetime import datetime
 import json
 from IPy import IP
+appVersion="1.0.0.0"
 print("Starting Identify. . . ")
 
 from geopy.geocoders import Nominatim
@@ -308,3 +309,4 @@ def monitor_summary(loop_amount, total_loop_amount, result):
 
 if __name__ == '__main__':
     menu()
+
